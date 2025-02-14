21ac5adb342d68d5723b154e5bfc2beb0fe2d439
Bridget Amana <amanabridget03@gmail.com>
2025-02-14 10:12:01 +0100

    feat: add Navbar component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import FirstStep from "./component/FirstStep";
+import Navbar from "./component/Navbar";
 
 function App() {
   return (
-    <div className="flex flex-col justify-center items-center">
-      <FirstStep />
+    <div className="pb-16 px-5 flex flex-col justify-center items-center font-roboto" style={{
+      background: `linear-gradient(0deg, #02191D, #02191D),
+      radial-gradient(52.52% 32.71% at 50% 97.66%, rgba(36, 160, 181, 0.2) 0%, rgba(36, 160, 181, 0) 100%)`
+    }}>
+      <Navbar />
     </div>
   );
 }
