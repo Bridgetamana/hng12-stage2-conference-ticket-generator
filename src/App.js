bb0aca9e49e6a1f07ea12a97b62105230a5fe944
Bridget Amana <amanabridget03@gmail.com>
2025-02-14 10:13:41 +0100

    feat: update FirstStep component

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ function App() {
       radial-gradient(52.52% 32.71% at 50% 97.66%, rgba(36, 160, 181, 0.2) 0%, rgba(36, 160, 181, 0) 100%)`
     }}>
       <Navbar />
+      <FirstStep />
     </div>
   );
 }
